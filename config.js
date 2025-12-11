bd3b956b4633a6b23e7a5b65ff5ca2b08d843e8c
onnurinoah <ixtys0704@gmail.com>
2025-12-11 16:48:38 +0900

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlDvuA4NCjQxfH2Z1wUI89jybVGjLZHBA",
+  authDomain: "memory-984c9.firebaseapp.com",
+  projectId: "memory-984c9",
+  storageBucket: "memory-984c9.firebasestorage.app",
+  messagingSenderId: "361490884024",
+  appId: "1:361490884024:web:b546dedbda87ca12555b4d",
+  measurementId: "G-YT8VY0THF1"
+};
